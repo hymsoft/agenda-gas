1ad8e6c98d34b90ee193baf23219836953f09d8f
hymsoft <hymsoft@gmail.com>
2022-10-01 23:35:58 -0300

    Creacion de la funcion para mostrar la tabla contactos

--- funciones.js
+++ funciones.js
@@ -7,3 +7,11 @@ function doGet() {
 function obtenerDatosHTML(nombre) {
   return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos() {
+  let hoja = SpreadsheetApp.openById(
+    "17RgtnSL8Jh6mSBY4ENV5y9fnfmX__3ybwtpPTGpeGwU"
+  ).getActiveSheet();
+  let datos = hoja.getDataRange().getValues();
+  return datos;
+}
