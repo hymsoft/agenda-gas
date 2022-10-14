adb9d8af70e188986d1b36401879599ff3568bcc
hymsoft <hymsoft@gmail.com>
2022-10-14 19:55:42 -0300

    Modificada funcion para leer y guardar la imagen de contacto cuando este se esta editando. Ahora los botones del menu principal son iconos

--- funciones.js
+++ funciones.js
@@ -26,40 +26,25 @@ function obtenerDatos() {
 }
 
 function insertarContacto(contacto, imagen) {
-  if(imagen){
-    let blob = Utilities.newBlob(imagen.datos,imagen.tipo, imagen.nombre);
-    let archivo = CARPETA_DRIVE.createFile(blob);
-    contacto.imagen = CABECERA_URL_IMAGEN_DRIVE + archivo.getId();
-
-  }
-  HOJA.appendRow([contacto.nombre, contacto.apellido, contacto.correo, contacto.telefono,contacto.imagen]);
+  if (imagen) contacto.imagen = guardarImagen(imagen);
+  HOJA.appendRow([contacto.nombre, contacto.apellido, contacto.correo, contacto.telefono, contacto.imagen]);
 }
 
-function eliminarContacto(nFila){
+function modificarContacto(contacto, imagen) {
+  let celdas = HOJA.getRange(contacto.fila, 1, 1, HOJA.getLastColumn());
+  if (imagen) contacto.imagen = guardarImagen(imagen);
+
+  celdas.setValues([[contacto.nombre, contacto.apellido, contacto.correo, contacto.telefono, contacto.imagen]]);
+}
+
+function guardarImagen(imagen) {
+  let blob = Utilities.newBlob(imagen.datos, imagen.tipo, imagen.nombre);
+  let archivo = CARPETA_DRIVE.createFile(blob);
+  return CABECERA_URL_IMAGEN_DRIVE + archivo.getId();
+
+}
+
+function eliminarContacto(nFila) {
   HOJA.deleteRow(nFila);
 }
-
-function modificarContacto(nFila,datos){
-  let celdas  = HOJA.getRange(nFila, 1, 1, HOJA.getLastColumn()-1 );
-  celdas.setValues([[datos.nombre,datos.apellido,datos.correo,datos.telefono]]);  
-}
-
-function importarContactos(){
-  let url = "https://randomuser.me/api/?results=5&nat=es&inc=name,email,phone,picture";
-  let respuesta = UrlFetchApp.fetch(url).getContentText();
-  let datos = JSON.parse(respuesta);
-  datos.results.forEach(contacto => insertarContactoImportado(contacto));
-}
-
-function insertarContactoImportado(contacto){
-  let nombre = contacto.name.first;
-  let apellido = contacto.name.last;
-  let correo = contacto.email;
-  let telefono = contacto.phone;
-  let fotografia = contacto.picture.large;
-
-  insertarContacto(nombre,apellido,correo,telefono,fotografia);
-}
-
-
 
